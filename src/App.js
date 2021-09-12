f66c9d9628c3bd553adac3c8ce1ec0b2338dcbd2
Mehmet Alperen <me.alperen@gmail.com>
2021-09-12 14:35:08 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from './components/SignupForm';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Form = styled.div`
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+`;
